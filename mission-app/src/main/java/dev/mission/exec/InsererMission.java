214d6ffe0cab105444d6e74ff611883e87bbc63f
peac178 <yvan.pallies@laposte.fr>
2022-01-11 16:53:36 +0100

    refactor

--- mission-app/src/main/java/dev/mission/exec/InsererMission.java
+++ mission-app/src/main/java/dev/mission/exec/InsererMission.java
@@ -21,12 +21,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Controller
 @Profile("insert")
 public class InsererMission implements CommandLineRunner {
-    private Logger LOGGER = LoggerFactory.getLogger(InsererMission.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(InsererMission.class);
     private final MissionRepository repository;
-    // numéro de la Mission généré automatiquement
-    private AtomicInteger num;
     //        La date du jour LocalDate.now() pour l'exemple = 2020.01.01
-    private final LocalDate  toDay =LocalDate.of(2020,01,01);
+    private final LocalDate toDay = LocalDate.of(2020, 01, 01);
+
     public InsererMission(MissionRepository missionRepository) {
         repository = missionRepository;
     }
@@ -35,30 +34,35 @@ public class InsererMission implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 //        launchCreateMission();
-        ListerProchainesMissions().forEach(m->{
-            LOGGER.info("avec la sélection Date {}",m.toString());
-        });;
-        ListerProchainesMissionsParTJM(100.5).forEach(m->{
-            LOGGER.info("avec la sélection Date et Taux {}",m.toString());
-        });;
+        ListerProchainesMissions().forEach(m -> {
+            LOGGER.info("avec la sélection Date {}", m.toString());
+        });
+        ;
+        ListerProchainesMissionsParTJM(100.5).forEach(m -> {
+            LOGGER.info("avec la sélection Date et Taux {}", m.toString());
+        });
+        ;
     }
+
     public List<Mission> ListerProchainesMissions() {
         return repository.findAllByDateDebutAfter(toDay);
     }
-    private List<Mission> ListerProchainesMissionsParTJM(double taux){
-        return repository.findAllByDateDebutAfterAndTauxJournalierAfter(toDay,new BigDecimal(taux));
+
+    private List<Mission> ListerProchainesMissionsParTJM(double taux) {
+        return repository.findAllByDateDebutAfterAndTauxJournalierAfter(toDay, new BigDecimal(taux));
     }
+
     private void launchCreateMission() {
         // cherche l'id mission le plus grand de la BDD
         Optional<Mission> mission = this.repository.findAll()
                 .stream()
                 .max(Comparator.comparing(Mission::getId));
 
-        int initValue = mission.isPresent() ? mission.get().getId() : 0;
-        num = new AtomicInteger(initValue);
+        // numéro de la Mission généré automatiquement
+        int num = mission.isPresent() ? mission.get().getId() : 0;
         // crée d'autres mission
         for (int i = 0; i < 4; i++) {
-            Mission newMission = this.createMission(num.incrementAndGet());
+            Mission newMission = this.createMission(num++);
             this.repository.save(newMission);
             LOGGER.info(" Mission {} save ", newMission.getId());
         }
