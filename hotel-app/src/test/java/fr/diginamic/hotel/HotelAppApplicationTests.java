2d4ff32d9e07e7bceb701158baca74d5efb70a3d
peac178 <yvan.pallies@laposte.fr>
2022-01-13 11:55:45 +0100

    init

---
+++ hotel-app/src/test/java/fr/diginamic/hotel/HotelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
